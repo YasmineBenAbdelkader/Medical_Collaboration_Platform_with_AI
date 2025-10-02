886f5bb3b1cd7a113f01aba80796a39bd0eadf62
yasmine <benabdelkaderyasmine2@gmail.com>
2025-10-03 01:09:31 +0200

    Backend-register

--- backend/app/schemas/expert.py
+++ backend/app/schemas/expert.py
@@ -1,10 +1,38 @@
 from typing import Optional
 from .doctor import DoctorCreate, DoctorRead
+from typing import List
 
 class MedicalExpertCreate(DoctorCreate):
-    blog: Optional[str] = None
-    cv: Optional[str] = None
+    expertise_domain: str
+    expertise_type: str
+    expertise_description: str
+    expertise_certificate: str  
+    expertise_duration: str
+    official_nomination: str
+    publications: List[str] = []
+    cover_letter: str  
+    mission_availability: str
+    professional_number: str
+    professional_card: str
+    year_of_experience: str
+    curriculum_vitae: str
+    diploma: str
+    
 
 class MedicalExpertRead(DoctorRead):
-    blog: Optional[str] = None
-    cv: Optional[str] = None
+    expertise_domain: str
+    expertise_type: str
+    expertise_description: str
+    expertise_certificate: str  
+    expertise_duration: str
+    official_nomination: str
+    publications: List[str] = []
+    cover_letter: str  
+    mission_availability: str
+    professional_number: str
+    professional_card: str
+    year_of_experience: str
+    curriculum_vitae: str
+    diploma: str
+    
+    
